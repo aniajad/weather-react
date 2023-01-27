be4219d6fef9f076649fd2bd9f08152cde8cd407
aniajad <116589679+aniajad@users.noreply.github.com>
2023-01-27 19:08:11 +0100

    Updated h1

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1> Hello world</h1>
+        <h1>Weather App</h1>
         <Audio
           height="80"
           width="80"
